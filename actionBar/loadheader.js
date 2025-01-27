bdf00d2e63b61867c539edb288be3686255b85f0
NickReset <NickReset@github.com>
2025-01-26 21:58:10 -0500

    hover effect and logos

--- actionBar/loadheader.js
+++ actionBar/loadheader.js
@@ -1,11 +1,11 @@
-fetch('actionBar/actionBar.css')
+fetch('../../actionBar/actionBar.css')
     .then(response => response.text())
     .then(data => {
         let style = document.createElement('style');
         style.innerHTML = data;
         document.body.appendChild(style);
     });
-
-fetch('actionBar/actionBar.html')
+    
+fetch('../actionBar/actionBar.html')
     .then(response => response.text())
     .then(data => document.body.insertAdjacentHTML('afterbegin', data));
